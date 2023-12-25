1eff043dc91fa7a43b7d7d7d06ad86f8f40e3f16
Lenin Acosta <rkb.lenin@gmail.com>
2023-12-25 13:36:57 -0500

    dialog

---
+++ kivymd/basics/helpers.py
@@ -0,0 +1,10 @@
+hpusername = """
+MDTextField:
+    hint_text : "Username"
+    pos_hint : {"center_x":.5, "center_y":.5}
+    size_hint_x : None
+    width : 300
+    helper_text : "Enter your username"
+    icon_right : "account"
+    icon_right_color : app.theme_cls.primary_color
+"""
--- kivymd/basics/input.py
+++ kivymd/basics/input.py
@@ -1,28 +1,38 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDFlatButton
-from kivymd.uix.textfield import MDTextField
+from kivymd.uix.dialog import MDDialog
 from kivy.lang import Builder
+from helpers import hpusername
 
-username_builder = """
-MDTextField:
-    hint_text : "Username"
-    pos_hint : {"center_x":.5, "center_y":.5}
-    size_hint_x : None
-    width : 300
-    helper_text : "Enter your username"
-    icon_right : "account"
-    icon_right_color : app.theme_cls.primary_color
-"""
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Cyan"
         screen = Screen()
+        button = MDFlatButton()
         # 
-        username = Builder.load_string(username_builder)
+        button.text = "Show"
+        button.pos_hint = {"center_x": .5, "center_y": .4}
+        button.on_release = self.showUsername
         # 
-        screen.add_widget(username)
+        self.username = Builder.load_string(hpusername)
+        # 
+        screen.add_widget(self.username)
+        screen.add_widget(button)
         return screen
+
+    def showUsername(self):
+        close = MDFlatButton(text="Close")
+        close.on_release = self.closeDialog
+        self.dialog = MDDialog(buttons=[close])
+        self.dialog.title = "This is your username"
+        self.dialog.text = self.username.text
+        self.dialog.open()
+    
+    def closeDialog(self):
+        self.dialog.dismiss()
+        
+        
 
 MainApp().run()
