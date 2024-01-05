9666e0d04775bd1ca012bc0f8ee59a3be6b096fe
Lenin Acosta <rkb.lenin@gmail.com>
2024-01-04 22:07:43 -0500

    update dataTable

--- kivymd/basics/dataTable.py
+++ kivymd/basics/dataTable.py
@@ -7,14 +7,25 @@ class MainApp(MDApp):
     def build(self):
         screen = Screen()
         rows = []
-        for i in range(50):
+        for i in range(20):
             row = (f"item {i}", f"item {i+1}")
             rows.append(row)
         cols = [("food",dp(30)), ("food",dp(30))]
-        table = MDDataTable(column_data = cols, row_data = rows)
+        table = MDDataTable(
+            column_data = cols, 
+            row_data = rows,
+            check = True,
+            elevation = 1
+            )
         table.pos_hint = {"center_x":.5, "center_y":.5}
         table.size_hint = (.9, .7)
+        table.bind(on_check_press = self.check_press)
         screen.add_widget(table)
         return screen
+    
+    def check_press(self, dataTable, row:list):
+        print("hi")
+        print(dataTable)
+        print(row)
     
 MainApp().run()
